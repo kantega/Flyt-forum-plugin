0d732c60e5d7203c873f962b0ee95267ff826fed
Espen Fossen <Espen.Fossen@kantega.no>
2007-11-08 12:45:34 +0000

    Trigger to active or deactive exchange support.
    git-svn-id: https://subversion.kantega.no/svn/aksess/projects/aksess-modules/trunk@7494 497bca9e-fae7-0310-9681-f2327195b1a6

---
+++ modules/exchange/src/java/no/kantega/exchange/tags/GetActiveTag.java
@@ -0,0 +1,56 @@
+package no.kantega.exchange.tags;
+
+import com.intrinsyc.cdo.Session;
+import com.intrinsyc.cdo.AddressEntry;
+import com.intrinsyc.cdo.AddressEntryProxy;
+
+import javax.servlet.jsp.tagext.TagSupport;
+import javax.servlet.jsp.tagext.BodyTagSupport;
+import javax.servlet.jsp.tagext.BodyContent;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.JspTagException;
+import javax.servlet.http.HttpServletRequest;
+
+import no.kantega.exchange.util.ExchangeSession;
+import no.kantega.commons.log.Log;
+import no.kantega.commons.configuration.Configuration;
+import no.kantega.commons.exception.ConfigurationException;
+import no.kantega.publishing.common.Aksess;
+
+/**
+ * User: Espen A. Fossen @ Kantega
+ * Date: Oct 9, 2007
+ * Time: 12:15:06 PM
+ */
+public class GetActiveTag extends BodyTagSupport {
+    private static final String SOURCE = "exchange.GetActiveTag";
+
+    private String userid = null;
+
+    public int doStartTag() throws JspException {
+
+        return EVAL_BODY_AGAIN;
+    }
+
+    public int doAfterBody() throws JspException {
+
+
+        try {
+            Configuration config = Aksess.getConfiguration();
+            String active = config.getString("jintegra.exchange.active");
+
+            if ("true".equalsIgnoreCase(active)) {
+                bodyContent.writeOut(getPreviousOut());
+            } else {
+            }
+
+        } catch (Exception e) {
+            Log.error(SOURCE, e, null, null);
+            throw new JspTagException(SOURCE + ":" + e.getMessage());
+        }
+
+        return SKIP_BODY;
+    }
+
+}
