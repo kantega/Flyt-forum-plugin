68dac47dcd334f3b068c7f11e406d6b18d1027d4
malber <malber@kantega.no>
2012-11-06 12:13:45 +0000

    GLIK-220: Slightly changed the "show more" functionality on the forum posts.
    git-svn-id: https://subversion.kantega.no/svn/aksess/projects/aksess-modules/trunk@33696 497bca9e-fae7-0310-9681-f2327195b1a6

--- modules/forum/src/java/no/kantega/forum/tags/wall/FormatWallPostPreview.java
+++ modules/forum/src/java/no/kantega/forum/tags/wall/FormatWallPostPreview.java
@@ -1,5 +1,7 @@
 package no.kantega.forum.tags.wall;
 
+import no.kantega.commons.exception.ConfigurationException;
+import no.kantega.commons.log.Log;
 import no.kantega.commons.util.LocaleLabels;
 import no.kantega.publishing.common.Aksess;
 
@@ -10,9 +12,11 @@ import javax.servlet.jsp.tagext.SimpleTagSupport;
 import java.io.IOException;
 
 public class FormatWallPostPreview extends SimpleTagSupport {
-    
+    private static final String SOURCE = "FormatWallPostPreview";
+
     private String postbody;
     private int charsInBodyPreview = 200;
+    private int charsPerLine = 1;
 
     public void doTag() throws JspException, IOException {
         PageContext pageContext = ((PageContext)getJspContext());
@@ -22,20 +26,26 @@ public class FormatWallPostPreview extends SimpleTagSupport {
         // Dont want to split up html anchor in preview text. So must first check if a link is present in the preview text
         String linkStartTag = "<a";
         String linkEndTag = "</a>";
+        try {
+            charsPerLine = Aksess.getConfiguration().getInt("forum.post.charsperline", 1);
+        } catch (ConfigurationException e) {
+            Log.debug(SOURCE, "Fetching configuration");
+        }
         int indexOfFirstLink = postbody.indexOf(linkStartTag);
-        if( indexOfFirstLink < (charsInBodyPreview + 1) ) {
+        if( indexOfFirstLink < (charsInBodyPreview - charsPerLine) ) {
             int indexOfFirstLinkEnd = postbody.indexOf(linkEndTag);
             if (indexOfFirstLinkEnd > 0) {
                 charsInBodyPreview = charsInBodyPreview + ( (indexOfFirstLinkEnd + linkEndTag.length()) - indexOfFirstLink);
             }
         }
-        // Creating a preview if neccessary
+        // Creating a preview if neccessary, if the post does not fit within the limit, one line is removed in order to
+        // show "show more" tag so it will not use more space
         if (postbody.length() > charsInBodyPreview) {
             String linkText = LocaleLabels.getLabel("forum.wall.post.previewlink","forum", Aksess.getDefaultAdminLocale());
-            formattedPostBody.append(postbody.substring(0, charsInBodyPreview +1));
+            formattedPostBody.append(postbody.substring(0, charsInBodyPreview - charsPerLine));
             formattedPostBody.append("<span class=\"oa-forum-post-preview-more-indicator\">...</span>");
             formattedPostBody.append("<br><a href=\"#\" class=\"oa-forum-post-preview-show-full-body-link\">" + linkText + "</a><br>");
-            formattedPostBody.append("<span class=\"oa-forum-post-preview-hidden-post-body\">" + postbody.substring(charsInBodyPreview + 1) + "</span>");
+            formattedPostBody.append("<span class=\"oa-forum-post-preview-hidden-post-body\">" + postbody.substring(charsInBodyPreview - charsPerLine) + "</span>");
         } else {
             formattedPostBody.append(postbody);
         }
