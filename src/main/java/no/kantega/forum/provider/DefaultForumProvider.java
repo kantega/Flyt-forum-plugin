6ae0b7eae3c2698e942bde9150c510f5be577c2a
Marvin Bredal Lillehaug <marlil@kantega.no>
2015-05-11 11:53:13 +0200

    DefaultForumProvider, generics

--- src/main/java/no/kantega/forum/provider/DefaultForumProvider.java
+++ src/main/java/no/kantega/forum/provider/DefaultForumProvider.java
@@ -1,13 +1,12 @@
 package no.kantega.forum.provider;
 
-import no.kantega.publishing.forum.ForumProvider;
-import no.kantega.publishing.common.data.Content;
 import no.kantega.forum.dao.ForumDao;
-import no.kantega.forum.model.ForumCategory;
 import no.kantega.forum.model.Forum;
+import no.kantega.forum.model.ForumCategory;
+import no.kantega.publishing.common.data.Content;
+import no.kantega.publishing.forum.ForumProvider;
 
 import java.util.List;
-import java.util.Iterator;
 
 public class DefaultForumProvider implements ForumProvider {
     private ForumDao dao;
@@ -15,17 +14,12 @@ public class DefaultForumProvider implements ForumProvider {
     public String getForumsAsOptionList(long selected) {
         StringBuilder buffer = new StringBuilder();
 
-        List forumcategories = dao.getForumCategories();
+        List<ForumCategory> forumcategories = dao.getForumCategories();
 
-        for (Object forumcategory : forumcategories) {
-            ForumCategory c = (ForumCategory) forumcategory;
+        for (ForumCategory c : forumcategories) {
             buffer.append("<optgroup label=\"").append(c.getName()).append("\">");
 
-            Iterator j = c.getForums().iterator();
-
-            while (j.hasNext()) {
-                Forum forum = (Forum) j.next();
-
+            for (Forum forum : c.getForums()) {
                 buffer.append("<option value=\"").append(forum.getId()).append("\"");
                 if (forum.getId() == selected) {
                     buffer.append(" selected");
