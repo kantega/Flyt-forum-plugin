290e8454489dc3b37c51d615f2d84f59d55ed664
Kristian Myrhaug <kmyrhaug@gmail.com>
2015-06-22 12:30:34 +0200

    Indices on forum_post.owner, .postDate and .modifiedDate

---
+++ src/main/resources/META-INF/resources/WEB-INF/dbmigrate/forum/201506221220/all/thread_post_indices.sql
@@ -0,0 +1,3 @@
+CREATE INDEX idx_forum_post_owner on forum_post(owner);
+CREATE INDEX idx_forum_post_postDate on forum_post(postDate);
+CREATE INDEX idx_forum_post_modifiedDate on forum_post(modifiedDate);
